18b541799adab25e74a5a4f41857aeec8beeaf32
Casey Hill <97810619+0art-vandelay0@users.noreply.github.com>
2023-07-21 10:14:06 -0700

    Test 2 Order constuctor w/ properties Passed

--- Pierres.Tests/ModelTests/OrderTests.cs
+++ Pierres.Tests/ModelTests/OrderTests.cs
@@ -16,8 +16,27 @@ namespace Pierres.Tests.ModelTests
         [TestMethod]
         public void OrderConstructor_CreatesInstanceOfOrder_Order()
         {
-            Order newOrder = new Order("Test Title", "Test desccription", 25, "07-21-2023");
+            Order newOrder = new Order("Test Title", "Test description", 25, "07-21-2023");
             Assert.AreEqual(typeof(Order), newOrder.GetType());
+        }
+
+        [TestMethod]
+        public void OrderConstructor_ReturnsOrderProperies_Order()
+        {
+            string title = "Test Title";
+            string description = "Test description";
+            int price = 150;
+            string date = "07-21-2023";
+            Order newOrder = new Order(title, description, price, date);
+            string titleResult = newOrder.Title;
+            string descrptResult = newOrder.Description;
+            int priceResult = newOrder.Price;
+            string dateResult = newOrder.Date;
+            Assert.AreEqual(title, titleResult);
+            Assert.AreEqual(description, descrptResult);
+            Assert.AreEqual(price, priceResult);
+            Assert.AreEqual(date, dateResult);
+
         }
     }
 }
