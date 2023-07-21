bdc738942453b322ae8617124ccc7f1a0e181d32
Casey Hill <97810619+0art-vandelay0@users.noreply.github.com>
2023-07-21 12:22:36 -0700

    Add route to Vendor Index & New Vendor Form

---
+++ Pierres/Contollers/VendorsController.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Pierres.Models;
+
+namespace Pierres.Controllers
+{
+    public class VendorsController : Controller
+    {
+        [HttpGet("/vendors")]
+        public ActionResult Index()
+        {
+            List<Vendor> allVendors = Vendor.GetAll();
+            return View(allVendors);
+        }
+
+        [HttpGet("/vendors/new")]
+        public ActionResult New()
+        {
+            return View();
+        }
+    }
+}
